d38506dd95d93ecaf0ec65980c1dc21ccab55376
zumaichi <dzumaquero@gmail.com>
2026-03-29 09:53:15 +0200

    solo dist

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import checker from "vite-plugin-checker";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [checker({ typescript: true }), react()],
-});
